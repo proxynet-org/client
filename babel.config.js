a09e2618d0773fe091d41f27a042003e17d4cd11
Antonin <antonin.juquel@outlook.fr>
2022-11-17 22:55:33 +0100

    chore: add linter pre commit hook and moved files

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,25 @@
-module.exports = function(api) {
+const moduleResolver = [
+  'module-resolver',
+  {
+    root: ['./src'],
+    extensions: [
+      '.ios.ts',
+      '.android.ts',
+      '.ts',
+      '.ios.tsx',
+      '.android.tsx',
+      '.tsx',
+      '.jsx',
+      '.js',
+      '.json',
+    ],
+  },
+];
+
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: [moduleResolver],
   };
 };
