1e65ca24879b164d467fdcfb2ab5407914948e8f
Antonin <antonin.juquel@outlook.fr>
2023-01-24 23:17:11 +0100

    package: react-native-keyboard-aware-scroll-view

--- src/screens/auth/SignupScreen.tsx
+++ src/screens/auth/SignupScreen.tsx
@@ -7,6 +7,7 @@ import { Formik } from 'formik';
 import i18n from 'languages';
 import { SignupSchema } from 'schemas';
 import { DateTimePickerAndroid } from '@react-native-community/datetimepicker';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 export function SignupScreen() {
   const navigation = useNavigation<NavigationProp<AuthTabParams>>();
@@ -14,161 +15,153 @@ export function SignupScreen() {
   const { theme } = useTheme();
 
   return (
-    <View className="flex-1 items-center justify-center p-5">
-      <View className="w-full p-5">
-        <Text h1>{i18n.t('Signup')}</Text>
-        <Text>Please signup to continue.</Text>
+    <KeyboardAwareScrollView
+      style={{ backgroundColor: theme.colors.background }}
+    >
+      <View className="flex-1 items-center justify-center p-5">
+        <View className="w-full p-5">
+          <Text h1>{i18n.t('Signup')}</Text>
+          <Text>Please signup to continue.</Text>
+        </View>
+        <Formik
+          initialValues={{
+            fullName: '',
+            birthDate: '',
+            email: '',
+            phone: '',
+            password: '',
+            confirmPassword: '',
+          }}
+          onSubmit={(values) => console.log(values)}
+          validationSchema={SignupSchema}
+          validateOnMount={true}
+        >
+          {({
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            setFieldTouched,
+            touched,
+            values,
+            errors,
+          }) => (
+            <View className="w-full items-center justify-center">
+              <FormInput
+                name="fullName"
+                values={values}
+                errors={errors}
+                touched={touched}
+                handleChange={handleChange}
+                handleBlur={handleBlur}
+                setFieldTouched={setFieldTouched}
+                leftIconName="account-outline"
+                placeholder="FULL NAME"
+              />
+              <FormInput
+                name="birthDate"
+                values={values}
+                errors={errors}
+                touched={touched}
+                handleChange={handleChange}
+                handleBlur={handleBlur}
+                setFieldTouched={setFieldTouched}
+                leftIconName="cake-variant-outline"
+                placeholder="BIRTH DATE"
+                onFocus={(e) => {
+                  e.currentTarget.blur();
+                  DateTimePickerAndroid.open({
+                    value: new Date(),
+                    onChange: (e, date) =>
+                      date &&
+                      handleChange('birthDate')(date.toLocaleDateString()),
+                    mode: 'date',
+                  });
+                }}
+              />
+              <FormInput
+                name="phone"
+                values={values}
+                errors={errors}
+                touched={touched}
+                handleChange={handleChange}
+                handleBlur={handleBlur}
+                setFieldTouched={setFieldTouched}
+                leftIconName="phone-outline"
+                placeholder="PHONE"
+                keyboardType="phone-pad"
+                autoComplete="tel"
+                textContentType="telephoneNumber"
+              />
+              <FormInput
+                name="email"
+                values={values}
+                errors={errors}
+                touched={touched}
+                handleChange={handleChange}
+                handleBlur={handleBlur}
+                setFieldTouched={setFieldTouched}
+                placeholder="EMAIL"
+                leftIconName="email-outline"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                autoCorrect={false}
+                autoComplete="email"
+                textContentType="emailAddress"
+              />
+              <FormInput
+                name="password"
+                values={values}
+                errors={errors}
+                touched={touched}
+                handleChange={handleChange}
+                handleBlur={handleBlur}
+                setFieldTouched={setFieldTouched}
+                placeholder="PASSWORD"
+                secureTextEntry
+                leftIconName="lock-outline"
+              />
+              <FormInput
+                name="confirmPassword"
+                values={values}
+                errors={errors}
+                touched={touched}
+                handleChange={handleChange}
+                handleBlur={handleBlur}
+                setFieldTouched={setFieldTouched}
+                placeholder="CONFIRM PASSWORD"
+                secureTextEntry
+                leftIconName="lock-outline"
+              />
+              <Button
+                buttonStyle={{
+                  width: 250,
+                  height: 75,
+                }}
+                containerStyle={{ borderRadius: 50 }}
+                title={i18n.t('Signup')}
+                titleProps={{ style: { fontWeight: 'bold', fontSize: 20 } }}
+                onPress={() => {
+                  handleSubmit();
+                }}
+                disabled={Object.keys(errors).length > 0}
+              />
+              <TouchableOpacity>
+                <Text style={{ color: theme.colors.primary }}>
+                  Forgot Password ?
+                </Text>
+              </TouchableOpacity>
+            </View>
+          )}
+        </Formik>
+        <View className="flex-row items-end">
+          <Text>Already have an account ?</Text>
+          <TouchableOpacity onPress={() => navigation.navigate('SigninScreen')}>
+            <Text style={{ color: theme.colors.primary, fontWeight: 'bold' }}>
+              Sign in
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
-      <Formik
-        initialValues={{
-          firstName: '',
-          lastName: '',
-          birthDate: '',
-          email: '',
-          phone: '',
-          password: '',
-          confirmPassword: '',
-        }}
-        onSubmit={(values) => console.log(values)}
-        validationSchema={SignupSchema}
-        validateOnMount={true}
-      >
-        {({
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          setFieldTouched,
-          touched,
-          values,
-          errors,
-        }) => (
-          <View className="w-full items-center justify-center">
-            <FormInput
-              name="firstName"
-              values={values}
-              errors={errors}
-              touched={touched}
-              handleChange={handleChange}
-              handleBlur={handleBlur}
-              setFieldTouched={setFieldTouched}
-              leftIconName="account-outline"
-              placeholder="FIRST NAME"
-            />
-            <FormInput
-              name="lastName"
-              values={values}
-              errors={errors}
-              touched={touched}
-              handleChange={handleChange}
-              handleBlur={handleBlur}
-              setFieldTouched={setFieldTouched}
-              leftIconName="account-outline"
-              placeholder="LAST NAME"
-            />
-            <FormInput
-              name="birthDate"
-              values={values}
-              errors={errors}
-              touched={touched}
-              handleChange={handleChange}
-              handleBlur={handleBlur}
-              setFieldTouched={setFieldTouched}
-              leftIconName="cake-variant-outline"
-              placeholder="BIRTH DATE"
-              onFocus={(e) => {
-                e.currentTarget.blur();
-                DateTimePickerAndroid.open({
-                  value: new Date(),
-                  onChange: (e, date) =>
-                    date &&
-                    handleChange('birthDate')(date.toLocaleDateString()),
-                  mode: 'date',
-                });
-              }}
-            />
-            <FormInput
-              name="phone"
-              values={values}
-              errors={errors}
-              touched={touched}
-              handleChange={handleChange}
-              handleBlur={handleBlur}
-              setFieldTouched={setFieldTouched}
-              leftIconName="phone-outline"
-              placeholder="PHONE"
-              keyboardType="phone-pad"
-              autoComplete="tel"
-              textContentType="telephoneNumber"
-            />
-            <FormInput
-              name="email"
-              values={values}
-              errors={errors}
-              touched={touched}
-              handleChange={handleChange}
-              handleBlur={handleBlur}
-              setFieldTouched={setFieldTouched}
-              placeholder="EMAIL"
-              leftIconName="email-outline"
-              keyboardType="email-address"
-              autoCapitalize="none"
-              autoCorrect={false}
-              autoComplete="email"
-              textContentType="emailAddress"
-            />
-            <FormInput
-              name="password"
-              values={values}
-              errors={errors}
-              touched={touched}
-              handleChange={handleChange}
-              handleBlur={handleBlur}
-              setFieldTouched={setFieldTouched}
-              placeholder="PASSWORD"
-              secureTextEntry={true}
-              leftIconName="lock-outline"
-            />
-            <FormInput
-              name="confirmPassword"
-              values={values}
-              errors={errors}
-              touched={touched}
-              handleChange={handleChange}
-              handleBlur={handleBlur}
-              setFieldTouched={setFieldTouched}
-              placeholder="CONFIRM PASSWORD"
-              secureTextEntry={true}
-              leftIconName="lock-outline"
-            />
-            <Button
-              buttonStyle={{
-                width: 250,
-                height: 75,
-              }}
-              containerStyle={{ borderRadius: 50 }}
-              title={i18n.t('Signup')}
-              titleProps={{ style: { fontWeight: 'bold', fontSize: 20 } }}
-              onPress={() => {
-                handleSubmit();
-              }}
-              disabled={Object.keys(errors).length > 0}
-            />
-            <TouchableOpacity>
-              <Text style={{ color: theme.colors.primary }}>
-                Forgot Password ?
-              </Text>
-            </TouchableOpacity>
-          </View>
-        )}
-      </Formik>
-      <View className="flex-row items-end">
-        <Text>Already have an account ?</Text>
-        <TouchableOpacity onPress={() => navigation.navigate('SigninScreen')}>
-          <Text style={{ color: theme.colors.primary, fontWeight: 'bold' }}>
-            Sign in
-          </Text>
-        </TouchableOpacity>
-      </View>
-    </View>
+    </KeyboardAwareScrollView>
   );
 }
