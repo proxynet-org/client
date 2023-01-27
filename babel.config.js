c2641d76c567e400e3099bb41c85d3a06b0f8fb9
Antonin <antonin.juquel@outlook.fr>
2023-01-27 01:57:27 +0100

    package: add react-native-reanimated

--- babel.config.js
+++ babel.config.js
@@ -20,6 +20,10 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['nativewind/babel', moduleResolver],
+    plugins: [
+      'nativewind/babel',
+      'react-native-reanimated/plugin',
+      moduleResolver,
+    ],
   };
 };
