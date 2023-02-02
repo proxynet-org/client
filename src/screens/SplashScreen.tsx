805b78ed334934ddbc5c6db66e1e93902ec5a934
Antonin <antonin.juquel@outlook.fr>
2023-02-02 01:36:23 +0100

    fix: smol fixes

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -1,10 +1,15 @@
-import { View } from 'components';
+import { View } from 'react-native';
 import { Text } from '@rneui/themed';
 
 export function SplashScreen() {
   return (
-    <View>
-      <Text>SplashScreen</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+      }}
+    >
+      <Text center>SplashScreen</Text>
     </View>
   );
 }
